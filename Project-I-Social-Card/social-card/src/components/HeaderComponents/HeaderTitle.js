200f3e3b99a5a5a0600397d5ce40d9cc1531b423
ChelseaTolnai <ChelseaTolnai@gmail.com>
2018-12-20 15:57:15 -0500

    Added model.js to project.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Header.css';
 
+const moment = moment().format();
+
 const HeaderTitle = () => {
     return (
         <div className="HeaderTitle">
             <h1>Lambda School</h1>
             <h3>@LambdaSchool</h3>
             <h3>•</h3>
-            <h3>26 jan</h3>
+            <h3>{moment}</h3>
         </div>
     );
 };
