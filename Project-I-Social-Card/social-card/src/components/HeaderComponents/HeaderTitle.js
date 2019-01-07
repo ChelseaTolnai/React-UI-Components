3b309f1c5cc68e368d836efca60257a781412d89
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-01-07 15:20:58 -0500

    Refreactor header title component to ES6 function component.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -2,17 +2,15 @@ import React from 'react';
 import moment from 'moment';
 import './Header.css';
 
-class HeaderTitle extends React.Component {
-    render() {
-        return (
-            <div className="HeaderTitle">
-                <h1>Lambda School</h1>
-                <h3>@LambdaSchool</h3>
-                <h3>•</h3>
-                <h3>{moment().format("D MMM").toLowerCase()}</h3>
-            </div>
-        );
-    }
+const HeaderTitle = () => {
+    return (
+        <div className="HeaderTitle">
+            <h1>Lambda School</h1>
+            <h3>@LambdaSchool</h3>
+            <h3>&bull;</h3>
+            <h3>{moment().format("D MMM").toLowerCase()}</h3>
+        </div>
+    );
 };
   
 export default HeaderTitle;
